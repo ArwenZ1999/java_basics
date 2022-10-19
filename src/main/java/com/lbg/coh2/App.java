0c030805e03642ab593cc5366fd15d61ed81b55a
yawenGit <arwen.zhang@outlook.com>
2022-10-19 17:06:03 +0100

    loop until enter certain key

--- src/main/java/com/lbg/coh2/App.java
+++ src/main/java/com/lbg/coh2/App.java
@@ -7,35 +7,59 @@ import java.util.Scanner;
 
 /**
  * Hello world!
+ * 
+ * 
+ * 
+ * Write a program that 
+ * allows a user to enter a VAT rate, 
+ * followed by series of item prices
+ *  and it calculates the total cost. 
+ *  The calculation should be done in a function.  
+ *  After the total amount has been displayed,
+ *   the user should be prompted to continue with more calculations, 
+ *   or QUIT by typing QUIT.
+	
+When the app starts it should prompt the user to
+ QUIT by type QUIT or PRESS ENTER to continue entering prices
  *
  */
 public class App 
 {
 	public static void main(String[] args) throws IOException
 	{
+		System.out.print("Enter a cost, its vat or enter quit to exit: ");
+		System. out. println();
+		String lineRead;
+		boolean whileContinue=true;
+		Scanner sc=new Scanner(System.in);
 		
-		Scanner sc=new Scanner(System.in);  
-		System.out.print("Enter a cost and vat: ");   
-		double cost = sc.nextDouble();  
-		double vat = sc.nextDouble();
-		sc.close();  
-        App app=new App();
-		double result=app.create_vat(cost,vat);
-		//prints the number  
-		System.out.println(result); 
+		while (whileContinue )
+		{
+			lineRead=sc.next();
+			if(lineRead.equalsIgnoreCase("quit"))
+				break;
+			String cost = lineRead;  
+			String vat = sc.next();
+			//sc.close();  
+			double cost1=Double.parseDouble(cost); 
+			double vat1=Double.parseDouble(vat); 
+	        App app=new App();
+			double result=app.create_vat(cost1,vat1);
+			
+			System.out.println(result); 
+			prompt();
+			continue;
+		}	 		
+		System.out.print("end "); 
+	}	
+	private static void prompt() {
+		// TODO Auto-generated method stub
+		System.out.print("continue with more calculation:");
+		System. out. println();
 	}
-	
-	
-	//Write a program that allows a user to input the cost of an item, 
-	//the vat rate and prints out the
-	//total price. The functionality that calculates the VAT should be in function.
-	
-	//Use the Scanner class to read data from the keyboard.
+
 	double create_vat(double cost, double vat)
 	{
 		return cost*vat;
 	}
-	
 }
-//stataic:it's not an object 
-//void: not put anything, no return value 
