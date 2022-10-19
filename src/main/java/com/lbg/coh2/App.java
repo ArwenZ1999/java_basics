2bff3e88050a2440b3fac4f1adb931c91c4bccc2
yawenGit <arwen.zhang@outlook.com>
2022-10-19 15:10:53 +0100

    scanner input

--- src/main/java/com/lbg/coh2/App.java
+++ src/main/java/com/lbg/coh2/App.java
@@ -1,8 +1,9 @@
 package com.lbg.coh2;
 
-import java.io.BufferedReader;
+//import java.io.BufferedReader; //control+shif+o
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -12,14 +13,27 @@ public class App
 {
 	public static void main(String[] args) throws IOException
 	{
-		System.out.print("enter a key:" );
-		//system.in: return askii number
 		
-		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-		
-		String dataRead=br.readLine();
-		
-		System.out.println("=>"+dataRead);
+		Scanner sc=new Scanner(System.in);  
+		System.out.print("Enter a cost and vat: ");   
+		double cost = sc.nextDouble();  
+		double vat = sc.nextDouble();
+		sc.close();  
+        App app=new App();
+		double result=app.create_vat(cost,vat);
+		//prints the number  
+		System.out.println(result); 
+	}
+	
+	
+	//Write a program that allows a user to input the cost of an item, 
+	//the vat rate and prints out the
+	//total price. The functionality that calculates the VAT should be in function.
+	
+	//Use the Scanner class to read data from the keyboard.
+	double create_vat(double cost, double vat)
+	{
+		return cost*vat;
 	}
 	
 }
