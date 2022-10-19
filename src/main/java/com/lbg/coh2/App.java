3c2a0a60fe8a9a932c44d6d5ba7355c4d9334069
yawenGit <arwen.zhang@outlook.com>
2022-10-19 14:35:46 +0100

    moved code to using readers and streams

--- src/main/java/com/lbg/coh2/App.java
+++ src/main/java/com/lbg/coh2/App.java
@@ -1,43 +1,27 @@
 package com.lbg.coh2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-	//static means you don't need to crate an object 
-    public static void main( String[] args ) //belong to class. static can only call static 
-    {
-        System.out.println( "Hello World!" );
-        int x; //declaration 
-        x=3;  // assignment
-        int y=4; // decl with assinment 
-        //local: scope in a function 
-        System.out.println(x);
-        App theApp=new App(); //App is a type, or class// this is how to create an object 
-        theApp.foobar();
-        double result=theApp.sum(3.6, 5);
-        System.out.print(result);
-      //  fooBar();
-    }
-    
-    static void foobar()//create method belong to object, objext can evoke both, class can only evoke class
-    {
-    	
-    	System.out.print("selv:, ");
-    	System.out.println("hi ");     	
-    }
-    int sum(int lh, int rh)
-    {
-       return lh+rh;
-    }
-    
-    double sum(double lh, double rh)
-    {
-       return (int)(lh+rh);
-    }
-   
+	public static void main(String[] args) throws IOException
+	{
+		System.out.print("enter a key:" );
+		//system.in: return askii number
+		
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		
+		String dataRead=br.readLine();
+		
+		System.out.println("=>"+dataRead);
+	}
+	
 }
 //stataic:it's not an object 
 //void: not put anything, no return value 
